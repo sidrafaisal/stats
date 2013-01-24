93872bed8dec68120a4a403221664f3d3214d2c3
sidrafaisal <sidra.aslam@pucit.edu.pk>
2013-01-24 14:52:56 +0100

    Jan 24, commit

--- src/main/java/LOD/App.java
+++ src/main/java/LOD/App.java
@@ -1,6 +1,5 @@
 package LOD;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,20 +9,27 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
-//import com.hp.hpl.jena.rdf.model.Model;
-//import com.hp.hpl.jena.rdf.model.ModelFactory;
+//import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 //import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.util.FileManager;
 //import com.hp.hpl.jena.vocabulary.RDFS;
 
-import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+//import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 //import com.hp.hpl.jena.rdf.model.Property;
 
+import com.hp.hpl.jena.ontology.*;
+//import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.util.iterator.Filter;
+
+import java.io.PrintStream;
+import java.util.*;
 
 import java.io.*;
-import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.vocabulary.*;
+//import com.hp.hpl.jena.rdf.model.*;
+//import com.hp.hpl.jena.vocabulary.*;
 
 
 //import java.io.BufferedReader;
@@ -31,6 +37,8 @@ import com.hp.hpl.jena.vocabulary.*;
 //import java.io.IOException;
 //import java.io.InputStream;
 //import java.io.InputStreamReader;
+//sparql.function.library;
+
 
 public class App
 {
@@ -39,48 +47,47 @@ public class App
 		model = ModelFactory.createDefaultModel();
 	int i=0;
     	// use the FileManager to find the input file
-   	 try{
+   	 try{  
 		InputStream in = FileManager.get().open("ami.xml");
    		  model.read(in," ");
-   		  
-		  model.write(System.out);
-		  /*StmtIterator iter = model.listStatements( new SimpleSelector( null, null, (RDFNode)null ) );  
-		  while (iter.hasNext())
-		  {
-			  i++;
-			  iter.next();			  
-		  }
-		  System.out.println("has="+i);
-		  */
-		  System.out.println("Statistics:");
-		 
-		  System.out.println("triples = "+ getTriplesCount());
-		  System.out.println("Properties = "+ getPropertiesCount());
-		  System.out.println("entities = "+getEntitiesCount());
-		  System.out.println("distinct entities = "+getDistinctEntitiesCount());
-		  /*System.out.println("distinct resource URIs = "+getDistinctResourceURIsCount());
-		  
-		  System.out.println("instantiated classes = "+ getInstantiatedClassesCount());
-		 		  
-		  System.out.println("distinct subject nodes = "+getDistinctSubjectCount());
-		  System.out.println("distinct object nodes = "+getDistinctObjectCount());
 
-		  System.out.println("instances of the class = "+getClassInstancesCount());
-		  System.out.println("distinct subjects in triples using the property= "+getDistSubjUsingPropertyCount());
-		  System.out.println("triples using the property = "+getTriplesUsingPropertyCount());
+
+		  PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+		  writer.println("Statistics:");
+		  writer.println("");
 		  
-		  System.out.println("distinct objects in triples using the property = "+getDistObjUsingPropertyCount());
-		  System.out.println("classes used = "+getClassesUsedCount());
-		  System.out.println("literals = "+ getLiteralsCount());*/
-  
+		  writer.println("triples = "+getTriplesCount());
+		  writer.println("properties = "+ getPropertiesCount());
+		  writer.println("distinct entities = "+getDistinctEntitiesCount());
+		  writer.println("entities = "+getEntitiesCount());
+		  writer.println("distinct resource URIs = "+getDistinctResourceURIsCount());		  
+		  writer.println("instantiated classes = "+ getInstantiatedClassesCount());
+		 		  
+		  writer.println("distinct subject nodes = "+getDistinctSubjectCount());
+		  writer.println("distinct object nodes = "+getDistinctObjectCount());
+
+		  writer.println("instances of the class = "+getClassInstancesCount());
+		  writer.println("classes used = "+getClassesUsedCount());
+		  writer.println("literals = "+ getLiteralsCount()); 
+		  writer.println("Blank subjects = "+ getBlankSubjectCount());
+		  writer.println("Blank objects = " +getBlankObjectCount());
+		  writer.println("distinct subjects in triples using the property= "+getDistSubjUsingPropertyCount());
+		  writer.println("triples using the property = "+getTriplesUsingPropertyCount());
+		  
+		  writer.println("distinct objects in triples using the property = "+getDistObjUsingPropertyCount());
+		  writer.println("typed string length = "+getTypedStringLength());
+		  writer.println("untyped string length = "+getUntypedStringLength());
+		  writer.println("typed subjects = "+ getTypedSubjectCount());
+		  writer.println("labeled subjects " +getLabeledSubjectCount());
+		  writer.println("links " +getlinksCount());
+	//	  writer.println("labeled subjects " +getLabeledSubjectCount());
+		  
+		  writer.close();		   
    	 }
 		 catch(Exception e){ System.out.println("error-"+e);}		
 		
 }
-	  public List<String> getCount()
-	  {			  
-		  return ExecQuery("SELECT (count(DISTINCT ?type) As ?no) { ?s a ?type }");
-	  }
+
 	private Model model;
 	
     public static void main(String [] args) throws IOException
@@ -88,7 +95,10 @@ public class App
         App app = new App();
     
     }
- 
+	  public List<String> getlinksCount()
+	  {			  
+		  return ExecQuery("prefix ns: <http://example.org/ns#> SELECT (count(*) As ?no) { ?s ?p ?o filter(isIRI(?s) && isIRI(?o) && (?s)!=(?o))}");
+	  } 
     public List<String> getTriplesCount()
     {
     	  return ExecQuery("SELECT (COUNT(*) AS ?no) { ?s ?p ?o  }");
@@ -116,7 +126,27 @@ public class App
 	  {
 		  return ExecQuery("SELECT (COUNT(DISTINCT ?o ) AS ?no) {  ?s ?p ?o  filter(isLiteral(?o)) }");
 	  }
-
+	  
+	  public List<String> getTypedStringLength()
+	  {
+		  return ExecQuery("prefix xsd: <http://www.w3.org/2001/XMLSchema#> prefix fn: <http://www.w3.org/2005/xpath-functions#> SELECT (avg(fn:string-length(?o) ) AS ?no) {  ?s ?p ?o  filter(isLiteral(?o) && datatype(?o)=xsd:string) }");
+	  } 
+	  
+	  public List<String> getUntypedStringLength()
+	  {
+		  return ExecQuery("prefix xsd: <http://www.w3.org/2001/XMLSchema#> prefix fn: <http://www.w3.org/2005/xpath-functions#> SELECT (avg(fn:string-length(?o) ) AS ?no) {  ?s ?p ?o  filter(isLiteral(?o) && datatype(?o)='') }");
+	  }
+	  
+	  public List<String> getTypedSubjectCount()
+	  {			  
+		  return ExecQuery("SELECT (COUNT(DISTINCT ?s ) AS ?no) {  ?s a ?o }");
+	  }	  
+	  
+	  public List<String> getLabeledSubjectCount()
+	  {			  
+		  return ExecQuery("prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT (COUNT(DISTINCT ?s ) AS ?no) {  ?s ?p ?o filter(?p=rdfs:label)}");
+	  }	
+	  
 	  public List<String> getInstantiatedClassesCount()
 	  {
 		  /*We require classes to have URIs. Anonymous types do not count.*/
@@ -126,21 +156,31 @@ public class App
 	  {			  
 		  return ExecQuery("SELECT (count(DISTINCT ?type) As ?no) { ?s a ?type }");
 	  }
-	 
-	  public List<String> getDistinctSubjectCount()
-	  {			  
-		  return ExecQuery("SELECT (COUNT(DISTINCT ?s ) AS ?no) {  ?s ?p ?o   }");
-	  }
-		
-	  public List<String> getDistinctObjectCount()
-	  {			  
-		  return ExecQuery("SELECT (COUNT(DISTINCT ?o ) AS ?no) {  ?s ?p ?o  filter(!isLiteral(?o)) }");
-	  }
 
 	  public List<String> getClassInstancesCount()
 	  {			  
 		  return ExecQuery("SELECT  ?class (COUNT(?s) AS ?no ) { ?s a ?class } GROUP BY ?class ORDER BY ?no");
+	  }	 
+	  public List<String> getDistinctSubjectCount()
+	  {			  
+		  return ExecQuery("SELECT (COUNT(DISTINCT ?s ) AS ?no) {  ?s ?p ?o   }");
 	  }
+
+	  public List<String> getBlankSubjectCount()
+	  {			  
+		  return ExecQuery("SELECT (COUNT(DISTINCT ?s ) AS ?no) {  ?s ?p ?o   filter(isBlank(?s))}");
+	  }
+	  
+	  public List<String> getDistinctObjectCount()
+	  {			  
+		  return ExecQuery("SELECT (COUNT(DISTINCT ?o ) AS ?no) {  ?s ?p ?o  filter(!isLiteral(?o)) }");
+	  }
+	  
+	  public List<String> getBlankObjectCount()
+	  {			  
+		  return ExecQuery("SELECT (COUNT(DISTINCT ?o ) AS ?no) {  ?s ?p ?o  filter(isBlank(?o)) }");
+	  }
+
 	  public List<String> getDistSubjUsingPropertyCount()
 	  {			  
 		  return ExecQuery("SELECT  ?p (COUNT(DISTINCT ?s ) AS ?no ) { ?s ?p ?o } GROUP BY ?p ORDER BY ?no");
@@ -174,19 +214,5 @@ public class App
 		
 		return resultStrings;
     }
-   /*
-    public List<String> queryForPropertyValues(Resource property){
-    	String queryString = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  SELECT ?message WHERE  { ?s <" + property.getURI() +"> ?message}";
-    	Query query = QueryFactory.create(queryString);
-    	QueryExecution exec = QueryExecutionFactory.create(query, model);
-    	ResultSet rs = exec.execSelect();
-    	List<String> resultStrings = new ArrayList<String>();
-    	
-    	while(rs.hasNext()){
-    		QuerySolution sol = rs.next();
-    		resultStrings.add(sol.get("?message").asLiteral().getString());
-    	}
-    	
-    	return resultStrings;
-    }*/
+
     }
